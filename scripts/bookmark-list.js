3a17a61b061439fa7a1b3cea5e39558a921eb390
Robin Khiv <robinkhiv@gmail.com>
2019-01-30 08:01:22 -0800

    new stuff

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -26,25 +26,25 @@ const bookmarkList = (function () {
 
   const generateNewItemSubmitFormExpanded = function (){
     return `
-       <label>Title</label>
-          <input type="text" name="title" placeholder="Title"><br>
-          <label>URL</label>
-          <input type="text" name="url" placeholder="URL"><br>
-          <label>Description</label>
-          <input type="text" name="desc" placeholder="Description"><br>
-          <label>Rating</label>
-          <select name="rating">
+       <label for="bookmark-title">Title</label>
+          <input type="text" id="bookmark-title" name="title" placeholder="Title"><br>
+          <label for="bookmark-url">URL</label>
+          <input type="text" id="bookmark-url" name="url" placeholder="URL"><br>
+          <label for="bookmark-description">Description</label>
+          <input type="text" id="bookmark-description" name="desc" placeholder="Description"><br>
+          <label for="bookmark-rating">Rating</label>
+          <select id="bookmark-rating" name="rating">
               <option>1</option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
           </select><br>
-          <button type="submit" class="js-addBookmark-btn">Add Bookmark</button>`;
+          <button type="submit" name="submit" class="js-addBookmark-btn">Add Bookmark</button>`;
   }
 
-  function generateBookmarkString(shoppingList) {
-    const items = shoppingList.map((item) => generateItemElement(item));
+  function generateBookmarkString(bookmarkedItems) {
+    const items = bookmarkedItems.map((item) => generateItemElement(item));
     return items.join('');
   }
 
@@ -59,9 +59,11 @@ const bookmarkList = (function () {
                   <h3>${item.title}</h3>
                     <div class="${item.expanded ? "view" : "hidden"}">
                       <p>description: ${(!item.desc) ? 'no description avaiable': item.desc}</p>
-                      <a href="${item.url}">Visit Site</a>
+                      <a href="${item.url}">Visit Site</a><br>
                     </div>
                   <p>Rating: ${item.rating}</p>
+                  <button class="${item.expanded ? "view" : "hidden"} js-edit-button">Edit</button>
+                  <button class="js-bookmark-delete">Delete</button>
               </li>`
   }
   
