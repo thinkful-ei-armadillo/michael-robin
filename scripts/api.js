1cf69d71295823da365d6fb0884bad83171ba523
Robin Khiv <robinkhiv@gmail.com>
2019-01-31 10:40:01 -0800

    modified api script

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,5 @@
-/* global, store, index */
 'use strict';
+// eslint-disable-next-line no-unused-vars
 const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/michaelRobin';
   const getItems = function () {
