542e9917aa02548aba8a5b7b25fe1c47cedb8598
Robin Khiv <robinkhiv@gmail.com>
2019-01-31 10:49:05 -0800

    index js looks better

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-/* global bookmark-list, store,api,$ */
+/* global store,api,bookmarkList, $ */
+'use strict';
 $(document).ready(function () {
   api.getItems()
-   .then ( res  => res.json())
-   .then (resJ => {
-    resJ.forEach(item => 
-      store.addItem(item));
-    bookmarkList.bindEventListeners();
-    bookmarkList.render();}
-    )
+    .then(res => res.json())
+    .then(resJ => {
+      resJ.forEach(item =>
+        store.addItem(item));
+      bookmarkList.bindEventListeners();
+      bookmarkList.render();
+    });
 });
-  
-  
+
