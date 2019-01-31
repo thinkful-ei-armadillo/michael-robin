a976a4896e61f3338b68f3e7dafba6e83f2d85d9
Robin Khiv <robinkhiv@gmail.com>
2019-01-31 10:29:31 -0800

    api looks cleaner

--- scripts/api.js
+++ scripts/api.js
@@ -1,80 +1,55 @@
 /* global, store, index */
-
+'use strict';
 const api = (function () {
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/michaelRobin';
-    const getItems = function () {
-        return fetch(`${BASE_URL}/bookmarks`);
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/michaelRobin';
+  const getItems = function () {
+    return fetch(`${BASE_URL}/bookmarks`);
+  };
+
+  function listApiFetch(...args) {
+    let error = false;
+    return fetch(...args)
+      .then(res => !res.ok ? error = true : res.json())
+      .then(data => {
+        if (error) throw new Error(data.message);
+        return data;
+      })
+      .catch(err => alert(err.message));
+  }
+
+  const createItem = function (name) {
+    const options = {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: name,
     };
+    return listApiFetch(`${BASE_URL}/bookmarks`, options);
+  };
 
-    function listApiFetch(...args) {
-        let error = false;
-        return fetch(...args)
-            .then(res => {
-                if (!res.ok) {
-                    // Valid HTTP response but non-2xx status - let's flag an error!
-                    error = true;
-                }
-
-                // In either case, parse the JSON stream:
-                return res.json();
-            })
-
-            .then(data => {
-                // If error was flagged, throw an error with the JSON message
-                if (error) throw new Error(data.message);
-
-                // Otherwise, return the data (all promise chains 
-                // return promise objects)
-                return data;
-            })
-
-            .catch(err => alert(err.message));
-    }
-
-    const createItem = function (name) {
-        console.log(name);
-        const options = {
-            method: 'POST',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            }),
-            body: name,
-        }
-        return listApiFetch(`${BASE_URL}/bookmarks`, options);
-    }
-
-
-    const updateItem = function (id, updateData) {
-        const options = {
-            method: 'PATCH',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            }),
-            body: updateData,
-        };
-        return listApiFetch(`${BASE_URL}/bookmarks/${id}`, options);
-    }
-
-    const deleteItem = function (id) {
-        const options = {
-            method: 'DELETE',
-        };
-        return listApiFetch(`${BASE_URL}/bookmarks/${id}`, options);
+  const updateItem = function (id, updateData) {
+    const options = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: updateData,
     };
+    return listApiFetch(`${BASE_URL}/bookmarks/${id}`, options);
+  };
 
-    return {
-        getItems,
-        deleteItem,
-        // updateData,
-        createItem,
-        updateItem
-    }
+  const deleteItem = function (id) {
+    const options = {
+      method: 'DELETE',
+    };
+    return listApiFetch(`${BASE_URL}/bookmarks/${id}`, options);
+  };
 
-    // getItems,
-
-    // createItem,
-
-    // updateItem,
-
-    // deleteItem
+  return {
+    getItems,
+    createItem,
+    updateItem,
+    deleteItem
+  };
 }());
